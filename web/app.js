ecccfe3bf9a435efd5b59a9c92ec12ea628b60ee
B.J. Rossiter <b.j.rossiter@gmail.com>
2012-09-25 15:48:23 +1000

    [36605459,36605467] Admin can populate and order audio page posts in CMS

--- web/app.js
+++ web/app.js
@@ -5,19 +5,21 @@ $(document).ready(function() {
     dataType: 'jsonp',
     success: function(posts){
       var videos
-      var $navItemAfterVideos = $('#nav-item-after-video');
+      var $audioNav = $('#audio-nav-item');
       var $sectionAfterVideos = $('#audio-section');
       var maxHeight = 0, slider, $column;
+      var audio;
+      var $audioShare = $('#audio-share');
 
       _.chain(posts)
-        .select(function(video){ return _.find(video.categories, function(cat){ return cat === 'Videos'}); })
+        .select(function(video){ return _.find(video.categories, function(cat){ return cat === 'videos'}); })
         .sortBy(function(video, index){ return video.order; })
         .map(function(video, index){
-          $navItemAfterVideos.before('<li class="vid"><a data-page="' + (1 + index) + '">' + _.escape(video.title) + '</a></li>');
+          $audioNav.before('<li class="vid"><a data-page="' + (1 + index) + '">' + _.escape(video.title) + '</a></li>');
           $sectionAfterVideos.before(
             '<div class="section">' + 
               '<div class="video" data-vimeo-id="' + video.vimeo + '" >' + 
-                '<iframe class="player" width="1000px" height="1000px" src="http://player.vimeo.com/video/' + video.vimeo + '?api=1&player_id=player-' + video.vimeo + '" frameborder="0"></iframe>' +
+                '<iframe class="player" src="http://player.vimeo.com/video/' + video.vimeo + '?api=1&player_id=player-' + video.vimeo + '" frameborder="0"></iframe>' +
               '</div>' +
               '<div class="video-txt">' +
               '<p>' + video.content + '</p>' + 
@@ -28,8 +30,27 @@ $(document).ready(function() {
         })
         .value();
 
+      // audio page
+      audio = _.find(posts, function(post){ return _.find(post.categories, function(cat){ return cat === 'audio'}); });
+      $audioNav.find('a').text(audio.title);
+      $('#audio-title').text(audio.title);
+      $('#audio-content').text(audio.content);
+
+      // Soundclouds clips
+      _.chain(posts)
+        .select(function(post){ return _.find(post.categories, function(cat){ return cat === 'clips'}); })
+        .sortBy(function(clip, index){ return clip.order; })
+        .map(function(clip, index){
+          $audioShare.before(
+            '<li class="audio" style="background-image: url(' + clip.thumbnail + 
+            ');"><h2><a href="' + clip.soundcloud + '" class="sc-player"></a></h2></li>'
+          );
+        })
+        .value();
+
       // Enable sound player
       $('a.sc-player, div.sc-player').scPlayer();
+
 
       $('a.arrow.right').removeClass('hide');
       window.slider = slider = new Swipe(document.getElementById('container'), {
@@ -52,6 +73,9 @@ $(document).ready(function() {
           // Toggle soundcloud player
           $soundcloudPlayers = $(el).find('.sc-player');
           if ($soundcloudPlayers.length){
+            // Get rid of the play and pause text. We're doing this in js as setting
+            // a negative margin seems to mess with ipad
+            $soundcloudPlayers.find('.sc-play,.sc-pause').text('');
             $soundcloudPlayers.show();
           }else{
             $('.sc-player').hide();
