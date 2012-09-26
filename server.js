4c83f1cb5a76dd4d66b6c1c3f942e248d7951486
B.J. Rossiter <b.j.rossiter@gmail.com>
2012-09-26 14:43:54 +1000

    Setup basic auth when in deliver environment

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express')
 var app = express();
 
+function authorize(username, password) {
+  return 'tgm' === username && process.env.AUTHPASS === password;
+}
+
 app.configure(function(){
+  if (process.env.NODE_ENV === 'deliver'){
+    app.use(express.basicAuth(authorize));
+  }
   app.use(express.static(__dirname + '/web'));
 });
 
